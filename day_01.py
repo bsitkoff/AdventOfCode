2f8e1b4ebe61a103fbe2617f3b4d0bfad63aaaeb
Bridget Sitkoff <bridget_sitkoff@milton.edu>
2023-11-24 20:21:14 -0500

    Initial commit.

---
+++ day_01.py
@@ -0,0 +1,43 @@
+from collections import defaultdict, deque
+from typing import TYPE_CHECKING
+
+import aoc_helper
+from aoc_helper import (
+    Grid,
+    PrioQueue,
+    SparseGrid,
+    decode_text,
+    extract_ints,
+    extract_iranges,
+    extract_ranges,
+    extract_uints,
+    frange,
+    irange,
+    iter,
+    list,
+    map,
+    range,
+    search,
+    tail_call,
+)
+
+raw = aoc_helper.fetch(1, 2023)
+def parse_raw(raw: str):
+    pass
+    return(raw)
+
+def part_one(data):
+    pass
+    return(0)
+
+def part_two(data):
+    pass
+    return(0)
+
+data = parse_raw(raw)
+p1 = part_one(data)
+p2 = part_two(data)
+
+print(f'Submitting {p1} and {p2} as solutions.')
+aoc_helper.submit(1,1,p1,2023)
+aoc_helper.submit(1,2,p2,2023)
